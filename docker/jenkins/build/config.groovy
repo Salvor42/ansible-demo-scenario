24fd835d5709dc43f8eb0d62f32eaa2a478dd558
Axel Miesen <miesen@quadraginta-duo.de>
2019-05-25 08:34:19 +0200

    Added Jenkins

---
+++ docker/jenkins/build/config.groovy
@@ -0,0 +1,17 @@
+// Siehe https://technologyconversations.com/2017/06/16/automating-jenkins-docker-setup/comment-page-1/
+
+import jenkins.model.*
+import hudson.security.*
+import jenkins.security.s2m.AdminWhitelistRule
+ 
+def instance = Jenkins.getInstance()
+ 
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount("admin", "admin")
+instance.setSecurityRealm(hudsonRealm)
+ 
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+instance.setAuthorizationStrategy(strategy)
+instance.save()
+ 
+Jenkins.instance.getInjector().getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false)
